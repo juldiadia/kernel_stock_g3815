3e2f59e6a9e739e09536609a4c469837997d3ef2
Julian <july491@gmail.com>
2014-04-27 14:55:37 +0200

    Samsung opensource kernel

---
+++ config.sh
@@ -0,0 +1,1 @@
+make msm8930_cane_defconfig VARIANT_DEFCONFIG=msm8930_cane_wilcox_eur_lte_defconfig SELINUX_DEFCONFIG=selinux_defconfig
---
+++ kernel.sh
@@ -0,0 +1,3 @@
+export PATH=$PATH:/home/xpaum/android/gcc-linaro-4.7/bin
+
+make ARCH=arm CROSS_COMPILE=arm-eabi-
