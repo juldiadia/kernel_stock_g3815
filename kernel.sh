b8744f6f29f0ea3a7ede818fd98da11577adc08c
Julian Diaz <july491@gmail.com>
2015-08-03 18:58:57 +0200

    Fixing errors and Added config

--- kernel.sh
+++ kernel.sh
@@ -1,3 +1,3 @@
-export PATH=$PATH:/home/xpaum/android/arm-cortex_a15-toolchain/bin
+export PATH=$PATH:/home/xpaum/android/arm-toolchain/bin
 
-make ARCH=arm CROSS_COMPILE=arm-gnueabi-
+make ARCH=arm CROSS_COMPILE=arm-eabi- $1
